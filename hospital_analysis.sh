9d66271c4dcf95a2d66d75c7ec5ed83b99652581
isimbi <i.sonia@alustudent.com>
2026-09-25 22:42:10 +0200

    Add grep filter for CRITICAL rows in heart_rate and temperature logs

--- hospital_analysis.sh
+++ hospital_analysis.sh
@@ -1,4 +1,11 @@
 #!/bin/bash 
+
+process_vitals(){
+  grep "CRITICAL" active_logs/heart_rate.log active_logs/temperature.log
+}
+
+process_vitals
+
 water_audit(){  
   awk '
     /ICU_WATER_RESERVE/ {
@@ -17,4 +24,3 @@ water_audit(){
               }
 
 water_audit
-
